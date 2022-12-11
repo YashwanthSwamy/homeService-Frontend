25d9882fec1b5717b19f7620f7614cf1d0f420cb
YashwanthSwamy <yasw2875@colorado.edu>
2022-12-11 14:36:42 +0530

    feat: app routing module correction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookingComponent } from './booking/booking.component';
-import { CustomerInfoComponent } from './customer-info/customer-info.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { LogInComponent } from './Login/log-in/log-in.component';
-import { RegisterComponent } from './Login/register/register.component';
+import { AuthGuardService } from './authorizer/auth/authGuardService';
+import { FeatureGuardService } from './authorizer/auth/featureGuardService';
+
 
 const routes: Routes = [
-  {path: "", pathMatch: "full", redirectTo: "login"},
-  {path: "login", component: LogInComponent},
-  {path: "register", component: RegisterComponent},
-  {path: "homepage", component: HomepageComponent},
-  {path: "bookings", component: BookingComponent},
-  {path: "info", component: CustomerInfoComponent},
-
+  { path: "", redirectTo: "homepage", pathMatch: "full" },
+  {
+    path: "",
+    loadChildren: () =>
+      import("./feature/feature.module").then(m => m.FeatureModule),
+    canActivate: [FeatureGuardService],
+  },
+  {
+    path: "auth",
+    loadChildren: () =>
+      import("./authorizer/authorizer.module").then(m => m.AuthorizerModule),
+    canActivate: [AuthGuardService]
+  }
 ];
 
 @NgModule({
